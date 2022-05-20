af8ea1d45a386050b8d92c07c73daa368c007b3b
Mangpha <8350130@gmail.com>
2022-05-20 17:51:20 +0900

    Config Module & TypeORM Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,39 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UserModule } from './user/user.module';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.env',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().valid().required(),
+        DB_PORT: Joi.string().valid().required(),
+        DB_USER: Joi.string().valid().required(),
+        DB_PSWD: Joi.string().valid().required(),
+        DB_NAME: Joi.string().valid().required(),
+      }),
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PSWD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV === 'dev',
+      entities: [],
     }),
     UserModule,
   ],
