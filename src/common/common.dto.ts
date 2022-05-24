4b54e22f99606b7375a683134df9cc77a59ff19a
Mangpha <8350130@gmail.com>
2022-05-24 16:12:25 +0900

    Fix Common Dto

--- src/common/common.dto.ts
+++ src/common/common.dto.ts
@@ -1,4 +1,10 @@
-export interface CommonDto {
-  ok: boolean;
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class CommonDto {
+  @Field((type) => String)
+  status: string;
+
+  @Field((type) => String)
   error: string;
 }
